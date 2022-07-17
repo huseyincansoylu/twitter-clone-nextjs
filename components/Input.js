fb679cad8c7243feaceb5c83242ffd779df95030
huseyin <huseyin@keyko.io>
2022-07-17 17:46:10 +0300

    added post functionality

--- components/Input.js
+++ components/Input.js
@@ -1,6 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useRef, useState } from "react";
-
+import { db, storage } from "../firebase";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "@firebase/firestore";
+import { getDownloadURL, ref, uploadString } from "@firebase/storage";
 import {
   CalendarIcon,
   ChartBarIcon,
@@ -12,16 +20,56 @@ import {
 const Input = () => {
   const [input, setInput] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const filePickerRef = useRef();
 
-  const addImageToPost = () => {
-    console.log("ooo");
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
+
+  const sendPost = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db, "posts"), {
+      // id: session.user.uid,
+      // username: session.user.name,
+      // userImg: session.user.image,
+      // tag: session.user.tag,
+      text: input,
+      timestamp: serverTimestamp(),
+    });
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+    if (selectedFile) {
+      await uploadString(imageRef, selectedFile, "data_url").then(async () => {
+        const downloadUrl = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "posts", docRef.id), {
+          image: downloadUrl,
+        });
+      });
+    }
+
+    setLoading(false);
+    setInput("");
+    setSelectedFile(null);
   };
 
   return (
     <div
-      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}
+      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll ${
+        loading && "opacity-60"
+      }`}
     >
       <img
         src="https://avatars.githubusercontent.com/u/102976899?v=4"
@@ -46,7 +94,7 @@ const Input = () => {
               <div
                 onClick={() => setSelectedFile(null)}
                 className="absolute w-8 h-8 bg-[#15181c]
-             hover:bg-[#272c26] bg-opacity-75 rounded-full items-center justify-center top-1 left-1 cursor-pointer
+             hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer
             "
               >
                 <XIcon className="text-white h-5" />
@@ -60,39 +108,44 @@ const Input = () => {
           )}
         </div>
 
-        <div className="flex items-center justify-between pt-2.5">
-          <div className="flex items-center">
-            <div className="icon" onClick={() => filePickerRef.current.click()}>
-              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
-              <input
-                type="file"
-                onChange={addImageToPost}
-                ref={filePickerRef}
-                hidden
-              />
+        {!loading && (
+          <div className="flex items-center justify-between pt-2.5">
+            <div className="flex items-center">
+              <div
+                className="icon"
+                onClick={() => filePickerRef.current.click()}
+              >
+                <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+                <input
+                  type="file"
+                  onChange={addImageToPost}
+                  ref={filePickerRef}
+                  hidden
+                />
+              </div>
+
+              <div className="icon rotate-90">
+                <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
+
+              <div className="icon">
+                <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
+
+              <div className="icon">
+                <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
             </div>
 
-            <div className="icon rotate-90">
-              <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
-            </div>
-
-            <div className="icon">
-              <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
-            </div>
-
-            <div className="icon">
-              <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
-            </div>
+            <button
+              className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+              disabled={!input.trim() && !selectedFile}
+              onClick={sendPost}
+            >
+              Tweet
+            </button>
           </div>
-
-          <button
-            className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
-            disabled={!input.trim() && !selectedFile}
-            //onClick={sendPost}
-          >
-            Tweet
-          </button>
-        </div>
+        )}
       </div>
     </div>
   );
